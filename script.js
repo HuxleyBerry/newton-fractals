c716928895c716a3b61614c0b9820e62c50e3019
HuxleyBerry <huxley.berry@gmail.com>
2023-07-31 13:55:15 +0800

    tidy up a bit

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-var emptyColour = "black";
-var colours = ["rgb(255, 179, 0)", "rgb(128, 62, 117)", "rgb(255, 104, 0)", "rgb(166, 189, 215)", "rgb(193, 0, 32)", "rgb(206, 162, 98)", "rgb(129, 112, 102)", "rgb(0, 125, 52)", "rgb(246, 118, 142)", "rgb(0, 83, 138)", "rgb(255, 122, 92)", "rgb(83, 55, 122)", "rgb(255, 142, 0)", "rgb(179, 40, 81)", "rgb(244, 200, 0)", "rgb(127, 24, 13)", "rgb(147, 170, 0)", "rgb(89, 51, 21)", "rgb(241, 58, 19)", "rgb(35, 44, 22)"];
+let emptyColour = "black";
+let colours = ["rgb(255, 179, 0)", "rgb(128, 62, 117)", "rgb(255, 104, 0)", "rgb(166, 189, 215)", "rgb(193, 0, 32)", "rgb(206, 162, 98)", "rgb(129, 112, 102)", "rgb(0, 125, 52)", "rgb(246, 118, 142)", "rgb(0, 83, 138)", "rgb(255, 122, 92)", "rgb(83, 55, 122)", "rgb(255, 142, 0)", "rgb(179, 40, 81)", "rgb(244, 200, 0)", "rgb(127, 24, 13)", "rgb(147, 170, 0)", "rgb(89, 51, 21)", "rgb(241, 58, 19)", "rgb(35, 44, 22)"];
 
-var x = -10;
-var y = -10;
-var width = 20; //also height
-var fineness = 8;
-var iters = 100; //maximum iterations used of Newton's method
-var prec = 0.05; //precision
-var pixelSize = 2;
+let x = -10;
+let y = -10;
+let width = 20; //also height
+let fineness = 8;
+let pixelSize = 2;
 
+const iters = 100; //maximum iterations used of Newton's method
+const prec = 0.05; //precision
 
 
 //the polynomial roots
-var roots = [[2, 2], [2, -2], [-2, -2], [-2, 2]];
-var rootElements = [];
-var generalizationConstant = [1, 0];
+let roots = [[2, 2], [2, -2], [-2, -2], [-2, 2]];
+let rootElements = [];
+let generalizationConstant = [1, 0];
 
-var isRootSelected = false;
-var selectedRoot;
-var zoomSpeed = 2; //The factor by which the view changes when the mouse wheel is scrolled
+let isRootSelected = false;
+let selectedRoot;
+let zoomSpeed = 2; //The factor by which the view changes when the mouse wheel is scrolled
 
-var polynomialCoefficients = [];
-var derivativeCoefficients = [];
+let polynomialCoefficients = [];
+let derivativeCoefficients = [];
 
-var equationDisplayPrecision = 3; //number of decimal places
+let equationDisplayPrecision = 3; //number of decimal places
 
-var randomRoots = function (deg) {
+function randomRoots(deg) {
     roots = [];
-    for (var i = 0; i < deg; i++) {
+    for (let i = 0; i < deg; i++) {
         roots.push([x + Math.random() * width, y + Math.random() * width]);
     }
 }
 
 //Someone else's function to find all combinations of length k of a set
-var k_combinations = function (set, k) {
-    var i, j, combs, head, tailcombs;
+function k_combinations(set, k) {
+    let i, j, combs, head, tailcombs;
     if (k == 1) {
         combs = [];
         for (i = 0; i < set.length; i++) {
@@ -53,58 +53,58 @@ var k_combinations = function (set, k) {
     return combs;
 }
 
-var ac = function (a, b) {
+function ac(a, b) {
     return [a[0] + b[0], a[1] + b[1]];
 };
 
-var acs = function (nums) {
-    var sum = [0, 0];
-    for (var i = 0; i < nums.length; i++) {
+function acs(nums) {
+    let sum = [0, 0];
+    for (let i = 0; i < nums.length; i++) {
         sum = ac(sum, nums[i]);
     }
     return sum;
 };
 
-var mc = function (a, b) {
+function mc(a, b) {
     return [a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]];
 };
 
-var neg = function (a) {
+function neg(a) {
     return [-a[0], -a[1]]
 }
 
-var mcs = function (nums) {
-    var product = [1, 0];
-    for (var i = 0; i < nums.length; i++) {
+function mcs(nums) {
+    let product = [1, 0];
+    for (let i = 0; i < nums.length; i++) {
         product = mc(product, nums[i]);
     }
     return product;
 };
 
-var dc = function (a, b) { // a divided by b
+function dc(a, b) { // a divided by b
     return [(a[0] * b[0] + a[1] * b[1]) / (b[0] * b[0] + b[1] * b[1]), (a[1] * b[0] - a[0] * b[1]) / (b[0] * b[0] + b[1] * b[1])];
 };
 
-var ec = function (a, p) {
-    var product = [1, 0];
-    for (var i = 0; i < p; i++) {
+function ec(a, p) {
+    let product = [1, 0];
+    for (let i = 0; i < p; i++) {
         product = mc(product, a);
     }
     return product;
 };
 
-var sumOfProducts = function (n) {
-    var s = [0, 0];
-    var combs = k_combinations(roots, n);
-    for (var i = 0; i < combs.length; i++) {
+function sumOfProducts(n) {
+    let s = [0, 0];
+    let combs = k_combinations(roots, n);
+    for (let i = 0; i < combs.length; i++) {
         s = ac(s, mcs(combs[i]));
     }
     return s;
 };
 
-var getPolynomial = function () {
+function getPolynomial() {
     polynomialCoefficients = [];
-    for (var i = 0; i < roots.length; i++) {
+    for (let i = 0; i < roots.length; i++) {
         if ((i + roots.length) % 2 == 0) {
             polynomialCoefficients.push(sumOfProducts(roots.length - i));
         } else {
@@ -113,33 +113,33 @@ var getPolynomial = function () {
     }
     polynomialCoefficients.push([1, 0]);
     derivativeCoefficients = [];
-    for (var i = 1; i <= roots.length; i++) {
+    for (let i = 1; i <= roots.length; i++) {
         derivativeCoefficients.push(mc(polynomialCoefficients[i], [i, 0]));
     }
     derivativeCoefficients.push([0, 0]);
 }
 
-var pOfX = function (n) {
-    var ps = polynomialCoefficients[roots.length];
-    for (var i = roots.length - 1; i >= 0; i--) {
+function pOfX(n) {
+    let ps = polynomialCoefficients[roots.length];
+    for (let i = roots.length - 1; i >= 0; i--) {
         ps = ac(mc(ps, n), polynomialCoefficients[i]);
     }
     return ps;
 };
 
-var pPrimeOfX = function (n) {
-    var ps = derivativeCoefficients[roots.length];
-    for (var i = roots.length - 1; i >= 0; i--) {
+function pPrimeOfX(n) {
+    let ps = derivativeCoefficients[roots.length];
+    for (let i = roots.length - 1; i >= 0; i--) {
         ps = ac(mc(ps, n), derivativeCoefficients[i]);
     }
     return ps;
 };
 
-var newtonsMethod = function (num, iterations, precision) {
-    var result = num;
-    for (var i = 0; i < iterations; i++) {
+function newtonsMethod(num, iterations, precision) {
+    let result = num;
+    for (let i = 0; i < iterations; i++) {
         result = ac(result, mc(generalizationConstant, neg(dc(pOfX(result), pPrimeOfX(result)))));
-        var c = getColour(result, precision);
+        let c = getColour(result, precision);
         if (c != emptyColour) {
             return c;
         }
@@ -147,12 +147,12 @@ var newtonsMethod = function (num, iterations, precision) {
     return emptyColour;
 };
 
-var distance = function (a, b) {
+function distance(a, b) {
     return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
 };
 
-var getColour = function (n, precision) {
-    for (var i = 0; i < roots.length; i++) {
+function getColour(n, precision) {
+    for (let i = 0; i < roots.length; i++) {
         if (distance(n, roots[i]) <= precision) {
             return colours[i]
         }
@@ -160,8 +160,8 @@ var getColour = function (n, precision) {
     return emptyColour;
 };
 
-var getBracket = function (r) {
-    var b = "(z";
+function getBracket(r) {
+    let b = "(z";
     if (r[0] < 0) {
         b += " + " + (-r[0].toFixed(equationDisplayPrecision))
     } else if (r[0] > 0) {
@@ -178,7 +178,7 @@ var getBracket = function (r) {
     return b
 };
 
-var getTerm = function (coeff, exp) {
+function getTerm(coeff, exp) {
     t = ""
     if (coeff[0] > 0) {
         if (coeff[1] != 0) {
@@ -216,106 +216,106 @@ var getTerm = function (coeff, exp) {
     return t;
 }
 
-var cells = [];
-var canvas = document.getElementById("canvas");
+let cells = [];
+const canvas = document.getElementById("canvas");
 canvas.style.position = "absolute";
 canvas.style.left = "0px";
 canvas.style.top = "0px";
-var info = document.getElementById("info");
+const info = document.getElementById("info");
 info.style.position = "absolute";
 info.style.left = width * fineness * pixelSize * 1.1 + "px";
 info.style.top = "0px";
-var newFractalSlider = document.getElementById("new-fractal-slider");
+const newFractalSlider = document.getElementById("new-fractal-slider");
 newFractalSlider.value = 4;
-var newFractalButton = document.getElementById("new-fractal-button");
-var newFractalText = document.getElementById("new-fractal-text");
-var poly1 = document.getElementById("poly1");
-var poly2 = document.getElementById("poly2");
-var mouseCoordsText = document.getElementById("mousecoords");
-var finenessSlider = document.getElementById("fineness-slider");
+const newFractalButton = document.getElementById("new-fractal-button");
+const newFractalText = document.getElementById("new-fractal-text");
+const poly1 = document.getElementById("poly1");
+const poly2 = document.getElementById("poly2");
+const mouseCoordsText = document.getElementById("mousecoords");
+const finenessSlider = document.getElementById("fineness-slider");
 finenessSlider.value = 3;
-var finenessText = document.getElementById("fineness-text");
-var finenessButton = document.getElementById("fineness-button");
-var sizeSlider = document.getElementById("size-slider");
+const finenessText = document.getElementById("fineness-text");
+const finenessButton = document.getElementById("fineness-button");
+const sizeSlider = document.getElementById("size-slider");
 sizeSlider.value = 2;
-var sizeText = document.getElementById("size-text");
-var rootsDiv = document.getElementById("roots-div");
-var coloursButton = document.getElementById("colours-button");
+const sizeText = document.getElementById("size-text");
+const rootsDiv = document.getElementById("roots-div");
+const coloursButton = document.getElementById("colours-button");
 
-var magnitudeText = document.getElementById("magnitude-text");
-var magnitudeSlider = document.getElementById("magnitude-slider");
+const magnitudeText = document.getElementById("magnitude-text");
+const magnitudeSlider = document.getElementById("magnitude-slider");
 magnitudeSlider.value = 0;
-var angleText = document.getElementById("angle-text");
-var angleSlider = document.getElementById("angle-slider");
+const angleText = document.getElementById("angle-text");
+const angleSlider = document.getElementById("angle-slider");
 angleSlider.value = 0;
-var extendedButton = document.getElementById("extended-button");
+const extendedButton = document.getElementById("extended-button");
 
-finenessSlider.oninput = function () {
+finenessSlider.oninput = () => {
     finenessText.textContent = "Resolution: " + Math.pow(2, finenessSlider.value);
 };
-newFractalSlider.oninput = function () {
+newFractalSlider.oninput = () => {
     newFractalText.textContent = "Degree: " + newFractalSlider.value;
 };
-sizeSlider.oninput = function () {
+sizeSlider.oninput = () => {
     sizeText.textContent = "Size: " + sizeSlider.value;
 };
-angleSlider.oninput = function () {
-    angleText.textContent = "Angle: " + angleSlider.value + "�";
+angleSlider.oninput = () => {
+    angleText.textContent = "Angle: " + angleSlider.value + "°";
 };
 magnitudeSlider.oninput = function () {
     magnitudeText.textContent = "Magnitude: " + (0.01 * magnitudeSlider.value).toFixed(2);
 };
 
-var update = function () {
+function update() {
     fineness = (Math.pow(2, finenessSlider.value)) * (20 / width);
     pixelSize = 8 * sizeSlider.value / Math.pow(2, finenessSlider.value);
     info.style.left = width * fineness * pixelSize * 1.1 + "px";
     drawFractal();
 };
 
-var extendedUpdate = function () {
+function extendedUpdate() {
     generalizationConstant = [1 + magnitudeSlider.value * 0.01 * Math.cos(Math.PI * angleSlider.value / 180), magnitudeSlider.value * 0.01 * Math.sin(Math.PI * angleSlider.value / 180)];
     console.log(generalizationConstant);
     drawFractal();
 };
 
-var newRandomFractal = function () {
+function newRandomFractal() {
     randomRoots(newFractalSlider.value);
     drawFractal();
 };
 
-var drawFractal = function () {
-    var d = new Date();
+function drawFractal() {
+    let d = new Date();
     drawRoots();
     getPolynomial();
-    var ctx = canvas.getContext("2d");
-    var s = fineness * width;
+    let ctx = canvas.getContext("2d");
+    let s = fineness * width;
     canvas.width = s * pixelSize;
     canvas.height = s * pixelSize;
-    for (var i = 0; i < s; i++) {
-        for (var j = 0; j < s; j++) {
+    for (let i = 0; i < s; i++) {
+        for (let j = 0; j < s; j++) {
             ctx.fillStyle = newtonsMethod([x + i / fineness, y + j / fineness], iters, prec);
             ctx.fillRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize);
         }
     }
-    var p1 = "P(z) = ";
-    var p2 = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp= z<sup>" + roots.length + "</sup>";
-    for (var i = 0; i < roots.length; i++) {
+    let p1 = "P(z) = ";
+    let p2 = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp= z<sup>" + roots.length + "</sup>";
+    for (let i = 0; i < roots.length; i++) {
         p1 += getBracket(roots[i]);
         p2 += getTerm(polynomialCoefficients[roots.length - i - 1], roots.length - i - 1)
     }
     poly1.innerHTML = p1;
     poly2.innerHTML = p2;
-    var e = new Date();
+    let e = new Date();
     console.log(e.getTime() - d.getTime());
 }
 
-var drawRoots = function () {
+function drawRoots() {
     rootsDiv.innerHTML = "";
     rootElements = [];
-    for (var i = 0; i < roots.length; i++) {
+    for (let i = 0; i < roots.length; i++) {
         if (roots[i][0] < x + width && roots[i][1] < y + width) {
-            var newRoot = document.createElement("div");
+            let newRoot = document.createElement("div");
             newRoot.style.position = "absolute";
             newRoot.style.width = "2px";
             newRoot.style.height = "2px";
@@ -331,10 +331,10 @@ var drawRoots = function () {
     }
 }
 
-var moveRoot = function (e) {
-    var mousePos = [(e.clientX) / (fineness * pixelSize) + x, (e.clientY) / (fineness * pixelSize) + y];
+function moveRoot(e) {
+    let mousePos = [(e.clientX) / (fineness * pixelSize) + x, (e.clientY) / (fineness * pixelSize) + y];
     if (isRootSelected == false) {
-        for (var i = 0; i < roots.length; i++) {
+        for (let i = 0; i < roots.length; i++) {
             if (distance(mousePos, roots[i]) < width / 60) {
                 selectedRoot = i;
                 isRootSelected = true;
@@ -347,7 +347,7 @@ var moveRoot = function (e) {
     }
 };
 
-var moveRoot2 = function (e) {
+function moveRoot2(e) {
     if ((e.clientY) / (fineness * pixelSize) + y > 0) {
         mouseCoordsText.textContent = "Mouse position: " + ((e.clientX) / (fineness * 2) + x).toFixed(equationDisplayPrecision) + " + " + ((e.clientY) / (fineness * 2) + y).toFixed(equationDisplayPrecision) + "i";
     } else if ((e.clientY) / (fineness * pixelSize) + y < 0) {
@@ -361,17 +361,17 @@ var moveRoot2 = function (e) {
     }
 };
 
-var newColours = function () {
-    for (var i = colours.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = colours[i];
+function newColours() {
+    for (let i = colours.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = colours[i];
         colours[i] = colours[j];
         colours[j] = temp;
     }
     drawFractal();
 }
 
-var zoom = function (e) {
+function zoom(e) {
     console.log(e.clientX, e.clientY);
     if (e.deltaY > 0) {
         x = (x + (1 - zoomSpeed) * (e.clientX) / (fineness * pixelSize));
